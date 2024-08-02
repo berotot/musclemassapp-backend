b6128ea77917851138db72db64cb91a897fa45da
soerahmat69 <surahmatr38@gmail.com>
2024-08-03 02:25:40 +0800

    edit:routesdashb

--- routes/route.js
+++ routes/route.js
@@ -23,7 +23,7 @@ const router = require("express").Router();
 // ========================================== VISITOR ======================================================
 router.get("/api/v1/user/leaderboard", VerifyToken,LeaderBoardUsers);
 router.get("/api/v1/user/profile",VerifyToken, Profile);
-router.get("/api/v1/user", DashboardUser);
+router.get("/api/v1/user", VerifyToken,DashboardUser);
 router.post(
   "/api/v1/user/latihan/recomended/:_diff/:_type",VerifyToken,
   recomendedGetLatihan
