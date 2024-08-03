da6eea35ac04c64290bd0589d28c9e489b8a009d
soerahmat69 <surahmatr38@gmail.com>
2024-08-03 13:55:37 +0800

    edit:sttuscode

--- controllers/Admin/KelolaLatihan.js
+++ controllers/Admin/KelolaLatihan.js
@@ -38,8 +38,8 @@ module.exports = {
       return res
         .status(200)
         .send(ApiResponse("Success send data", true, 200, result));
-    } catch (error) {
-      return res.status(500).json({ message: "Ada problem nih " + error });
+    } catch (error) { 
+      return res.status(500).send(ApiResponse( "Ada problem nih " + error , false, 500, result));
     }
   },
   putDataLatihan: async (req, res) => {
